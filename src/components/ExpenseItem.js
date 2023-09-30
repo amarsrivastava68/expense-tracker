da83ac334279e8615251aed18f11e1d7966c6a57
Amar <amar@bbp.app>
2023-09-30 13:03:54 +0530

    https://github.com/amarsrivastava68/expense-tracker.git

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <>
+      <h2>ExpenseItem</h2>
+      <p>
+        Expense Items
+        <p>Food Rs 10 Petrol Rs 100</p>
+        Movies Rs 200
+      </p>
+    </>
+  );
+}
+
+export default ExpenseItem;
